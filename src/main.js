2903767043e71b9cee7cd14682349a429bf462d0
andypagdin <andyjpagdin@gmail.com>
2020-05-14 11:26:13 +0100

    Raycasting with orbit controls

--- src/main.js
+++ src/main.js
@@ -1,13 +1,22 @@
 import * as THREE from 'three'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
-// import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 import Cube from '../assets/models/cube.glb'
 import Anime from 'animejs/lib/anime.es.js'
 
 const HEIGHT = 700
 const WIDTH = 700
 
-// const Orbit = OrbitControls(THREE)
+const raycaster = new THREE.Raycaster()
+const mouse = new THREE.Vector2()
+let INTERSECTED
+
+function onMouseMove (event) {
+  event.preventDefault()
+  mouse.x = ( event.clientX / WIDTH ) * 2 - 1;
+	mouse.y = - ( event.clientY / HEIGHT ) * 2 + 1;
+}
+
 const scene = new THREE.Scene()
 const camera = new THREE.PerspectiveCamera(75, WIDTH / HEIGHT, 0.1, 1000)
 
@@ -15,7 +24,7 @@ const renderer = new THREE.WebGLRenderer()
 renderer.setSize(WIDTH, HEIGHT)
 document.body.appendChild(renderer.domElement)
 
-// const controls = new Orbit(camera, renderer.domElement)
+const controls = new OrbitControls(camera, renderer.domElement)
 
 const light = new THREE.AmbientLight(0x404040, 5)
 scene.add(light)
@@ -23,7 +32,6 @@ scene.add(light)
 const loader = new GLTFLoader()
 
 loader.load(Cube, function (gltf) {
-  console.log(gltf.scene.children[0])
   Anime({
     targets: gltf.scene.children[0].rotation,
     y: 5,
@@ -39,12 +47,39 @@ loader.load(Cube, function (gltf) {
 })
 
 camera.position.z = 5
-// controls.update()
+controls.update()
 
 function animate() {
   requestAnimationFrame(animate)
-  // controls.update()
+  controls.update()
+  render()
+}
+
+function render () {
+  raycaster.setFromCamera(mouse, camera)
+
+  const intersects = raycaster.intersectObjects(scene.children)
+
+  if (intersects.length > 0) {
+    if (INTERSECTED != intersects[0].object) {
+      if (INTERSECTED) {
+        INTERSECTED.material.color.set(INTERSECTED.currentHex)
+      }
+
+      INTERSECTED = intersects[0].object
+      INTERSECTED.currentHex = INTERSECTED.material.color.getHex()
+      INTERSECTED.material.color.set(0xff0000)
+    }
+  } else {
+    if (INTERSECTED) {
+      INTERSECTED.material.color.set(INTERSECTED.currentHex)
+    }
+    INTERSECTED = null
+  }
+
   renderer.render(scene, camera)
 }
+
+window.addEventListener('mousemove', onMouseMove, false)
 
 animate()
